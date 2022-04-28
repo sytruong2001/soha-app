6bb9e61fe6e8f234b9d19162421fddd2921e3021
sytruong2001 <vanyyy2001@gmail.com>
2022-04-28 18:16:23 +0700

    kh
    kh

---
+++ resources/views/user/Payment.blade.php
@@ -0,0 +1,60 @@
+@extends('layout.layoutUser')
+@section('content')
+    <div class="main main-raised">
+        <div class="profile-content">
+            <div class="container">
+                <div class="tab-content">
+                    <div class="row">
+                        <div class="col-md-2"></div>
+                        <div class="col-md-8"><img alt=""
+                                src="https://ssg.mediacdn.vn/soap/2022/03/16467099811_1920x466_optimized.jpg" width="100%">
+                        </div>
+                        <div class="col-md-2"></div>
+
+                    </div>
+
+                    <div class="tab-content">
+                        <div class="row">
+                            <div class="col-md-12 col-md-offset-1">
+                                <div class="row collections">
+                                    <div class="col-md-1">
+                                    </div>
+                                    <div class="col-md-10">
+                                        <div class="row">
+                                            <div class="col-md-5">
+                                                <div class="row">
+                                                    <div class="col-md-5">
+                                                        <h4 class="title"><img
+                                                                src="../assets/img/faces/christian.jpg" alt="Circle Image"
+                                                                class="img-responsive img-raised"
+                                                                style="border-radius: 15px">
+                                                        </h4>
+                                                    </div>
+                                                    <div class="col-md-7" style="background: black">
+                                                        <h4>ID của bạn là: </h4>
+                                                        <h4>ID của bạn là: </h4>
+                                                        <h4>ID của bạn là: </h4>
+                                                    </div>àafa
+                                                </div>
+
+
+                                            </div>
+                                            <div class="col-md-4" style="text-align:right">
+                                                <h4 class="title">A
+                                                </h4>
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                    <div class="col-md-1">
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
